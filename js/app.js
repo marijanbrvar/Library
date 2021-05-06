ad794c683e7c37aea4eeb790d1847338d716d60c
uwadonat <uwadonat@gmail.com>
2021-05-06 18:42:17 +0200

    add function definition

--- js/app.js
+++ js/app.js
@@ -1,9 +1,15 @@
 let myLibrary = [];
 
-function Book() {
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
 
 }
 
 function addBookToLibrary() {
+    let book = 
+
     
 }
