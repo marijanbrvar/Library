c2e61f70b616e76ead6d93b636123c7a2e18a4bd
uwadonat <uwadonat@gmail.com>
2021-05-07 15:34:24 +0200

    Add form string template and form reset

--- js/app.js
+++ js/app.js
@@ -1,10 +1,20 @@
 const addForm = document.querySelector('.add')
-console.log(addForm.title.value)
+const list = document.querySelector('.books')
+
 
 const addBookToLibrary = book => {
     const html = `
-
+    <div class="list-group-item">
+        <div class="d-flex w-100 justify-content-between">
+        <h5 class="mb-1 title"><small>Title: </small><br>${book.title}</h5>
+        <small class="pages">#pages: (${book.pages})</small>
+        </div>
+        <p class="mb-1 author"><small>Author: </small><br>${book.author}</p>
+        <small class="status">${book.read ? 'Readed' : 'To Read'}</small>
+        <small class="float-end remove">Remove</small>
+    </div>
     `
+    list.innerHTML += html
 }
 
 addForm.addEventListener('submit', e => {
@@ -17,6 +27,7 @@ addForm.addEventListener('submit', e => {
     )
 
     addBookToLibrary(book)
+    addForm.reset()
 
 })
 
