03df04d10b6b385d55af568abbec0e914e87fa78
uwadonat <uwadonat@gmail.com>
2021-05-07 15:26:18 +0200

    Add method for string template

--- js/app.js
+++ js/app.js
@@ -1,2 +1,29 @@
 const addForm = document.querySelector('.add')
-console.log(addForm)
+console.log(addForm.title.value)
+
+const addBookToLibrary = book => {
+    const html = `
+
+    `
+}
+
+addForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const book = new Book(
+        addForm.title.value.trim(),
+        addForm.author.value.trim(),
+        addForm.pages.value.trim(),
+        addForm.readed.checked
+    )
+
+    addBookToLibrary(book)
+
+})
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
