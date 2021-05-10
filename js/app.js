31e4724d34ddaa09ec5d43388ef9f9675ba818f5
Marijan Brvar <marijan.brvar@gmail.com>
2021-05-10 10:57:07 +0200

    Fix book list template to fetch data from array

--- js/app.js
+++ js/app.js
@@ -3,28 +3,33 @@ const list = document.querySelector('.books');
 const search = document.querySelector('.search input');
 const newBook = document.querySelector('.newbook');
 const hideForm = document.querySelector('.hideform');
-const readStatusToggle = document.querySelector('.status');
+
+let books = [{title: "The Lord of the Rings", author: "J. R. R. Tolkien", pages: 1197, read: false, id: 1}]
 
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
+  this.id = id
 }
 
-const addBookToLibrary = (book) => {
-  const html = `
-    <div class="list-group-item">
-        <div class="d-flex w-100 justify-content-between">
-        <h5 class="mb-1 title"><small>Title: </small><br>${book.title}</h5>
-        <small class="pages">#pages: (${book.pages})</small>
-        </div>
-        <p class="mb-1 author"><small>Author: </small><br>${book.author}</p>
-        <small class="status">${book.read ? 'Readed' : 'To Read'}</small>
-        <small class="float-end remove">Remove</small>
-    </div>
-    `;
-  list.innerHTML += html;
+const addBookToLibrary = () => {
+  list.innerHTML = `<div></div>`
+    books.forEach(book => {
+     list.innerHTML += `
+      <div class="list-group-item">
+          <div class="d-flex w-100 justify-content-between">
+          <h5 class="mb-1 title"><small>Title: </small><br>${book.title}</h5>
+          <small class="pages">#pages: (${book.pages})</small>
+          </div>
+          <p class="mb-1 author"><small>Author: </small><br>${book.author}</p>
+          <small class="status" onCLick="toggle(${book.id})">${book.read ? 'Read' : 'To Read'}</small>
+          <small class="float-end remove">Remove</small>
+      </div>
+      `;
+    })
+    console.log(books)
 };
 
 addForm.addEventListener('submit', (e) => {
